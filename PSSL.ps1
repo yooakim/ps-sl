d305df0b99db3e25c0ccec6a9152f9ad7fbb3757
Joakim Westin <joakim@jwab.net>
2012-08-20 09:14:29 +0200

    Skapade projektet på github

---
+++ PSSL.ps1
@@ -0,0 +1,175 @@
+﻿<#
+
+    Detta PowerShell script innehåller funktioner för att anropa Storstockholms Lokaltrafik (SL) webbservices för realtidsinformation.
+
+    
+  
+#>
+
+
+function Get-SLRealtidSite {            
+<#
+
+.SYNOPSIS
+
+sök fram en site, d.v.s. en hållplats med ett eller flera stoppställen. 
+
+Slussen är ett exempel på en site med flera stoppställen för olika sorters trafik
+
+
+.DESCRIPTION
+
+Metoden används för att söka fram en site, d.v.s. en hållplats med ett eller flera stoppställen. Slussen är ett exempel på en site med flera stoppställen för olika sorters trafik.
+
+Metoden tar emot ett argument, hållplatsnamn, och returnerar matchande siter.
+
+
+Metoden returnerar en lista med matchande siter. Listan kan innehåll inga, en eller flera siter. Varje site består av ett id-nummer (Number) och ett namn (Name).
+
+Metoden returnerar dessutom exekveringstid (ExecutionTime) för anropet samt eventuellt fel (HafasError).
+
+
+.PARAMETER stationSearch 
+
+Argumentet stationSearch kan ta emot en sträng med följande värden:
+
+ * Site-id, t.ex. 9192 för Slussen
+ * Fullständigt eller del av ett hållplatsnamn, t.ex. plan, Gullmarsplan. 
+
+
+.PARAMETER apiKey
+
+En giltig API nyckel 
+
+Läs mer om hur man får en API nyckel på http://www.trafiklab.se/kom-igang
+
+
+.EXAMPLE
+
+Letar efter site med namne 'Danderyds Sjukhus'
+
+    Get-SLRealtidSite 'Danderyds sjukhus' 61340ebe5448c815440ecbaf1c6b515e
+
+
+#>
+    [CmdletBinding(HelpURI='http://www.trafiklab.se/api/sl-realtidsinfo/GetSite')]       
+    param(
+        
+        [Parameter(Mandatory=$true,
+        ValueFromPipeline=$true,
+        ValueFromPipelineByPropertyName=$true)]
+        [Alias('hållplatsnamn')]
+        [ValidateNotNullorEmpty()]
+        [string]$query = "Slussen", 
+
+        [Parameter(Mandatory=$true,
+        ValueFromPipeline=$true,
+        ValueFromPipelineByPropertyName=$true)]
+        [Alias('key')]
+        [ValidateNotNullorEmpty()]
+        [string]$apiKey = $apiKey)
+
+    $uri = "https://api.trafiklab.se/sl/realtid/GetSite?stationSearch=$query&key=$apiKey"
+
+    (Invoke-RestMethod $uri).Hafas.Sites.Site
+}  
+
+function Get-SLRealtidDepartures {
+<#
+
+.SYNOPSIS
+
+Hämta avgångar i närtid för en specifik site för tunnelbana
+
+
+.DESCRIPTION
+
+Används för att hämta avgångar i närtid för en specifik site gällande tunnelbana. För övriga trafikslag så använd metoden Get-SLDpsDepartures.
+
+Metoden tar emot ett site-id och returnerar aktuella avgångar
+
+
+.PARAM siteId
+
+Unikt identifikationsnummer för den site som aktuella avgångar skall hämtas för, t.ex. 9192 för Slussen. Detta id fås från GetSite metoden
+
+
+.PARAMETER apiKey
+
+En giltig API nyckel 
+
+Läs mer om hur man får en API nyckel på http://www.trafiklab.se/kom-igang
+
+#>   
+    [CmdletBinding(HelpURI='http://www.trafiklab.se/api/sl-realtidsinfo/GetDepartures')]       
+    param(
+        [Parameter(Mandatory=$true,
+        ValueFromPipeline=$true,
+        ValueFromPipelineByPropertyName=$true)]
+        [Alias('ID')]
+        [int]$siteId, 
+        [Parameter(Mandatory=$true)]
+        [Alias('key')]
+        [string]$apiKey )
+    
+    process {
+        $uri = "https://api.trafiklab.se/sl/realtid/GetDepartures?siteId=$siteId&key=$apiKey"
+        (Invoke-RestMethod $uri).Departure
+
+    }
+     
+}
+
+
+function Get-SLRealtidDpsDepartures {
+ <#
+
+.SYNOPSIS
+
+Hämta avgångar för buss, pendeltåg, Roslagsbanan, Tvärbanan och Spårvagn från det nya systemet DPS. DPS skall i framtiden ersätta det gamla realtidssystemet och därmed också metoden GetDepartures. 
+
+
+.DESCRIPTION
+
+Används för att hämta avgångar i närtid för en specifik site gällande tunnelbana. För övriga trafikslag så använd metoden Get-SLDpsDepartures.
+
+Metoden tar emot ett site-id och returnerar aktuella avgångar
+
+
+.PARAM siteId
+
+Unikt identifikationsnummer för den site som aktuella avgångar skall hämtas för, t.ex. 9192 för Slussen. Detta id fås från GetSite metoden
+
+.PARAM timeWindow
+
+Hämta avgångar inom önskat tidsfönster. Där tidsfönstret är antalet minuter från och med nu. Giltiga värden är 10,30, 60, alla andra värden resulterar i ett fel. Om inget värde anges så används 30 minuter som default.
+
+
+.PARAMETER apiKey
+
+En giltig API nyckel 
+
+Läs mer om hur man får en API nyckel på http://www.trafiklab.se/kom-igang
+
+#>
+    [CmdletBinding(HelpURI='http://www.trafiklab.se/api/sl-realtidsinfo/GetDpsDepartures')]      
+    param(
+        [Parameter(Mandatory=$true,       
+        ValueFromPipeline=$true,
+        ValueFromPipelineByPropertyName=$true)]
+        [Alias('ID')]
+        [int]$siteId, 
+        [ValidateSet(10,30,60)] 
+        [int]$timeWindow = 30, 
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullorEmpty()]
+        [Alias('key')]
+        [string]$apiKey)
+    
+    process {
+        $uri = "https://api.trafiklab.se/sl/realtid/GetDpsDepartures?siteId=$siteId&timeWindow=$timeWindow&key=$apiKey"
+        (Invoke-RestMethod $uri).DPS
+
+    }
+     
+}
--- README.md
+++ README.md
@@ -1,4 +1,123 @@
-ps-sl
-=====
+# ps-sl
 
-PowerShell code to interact with AB Storstockholms Lokaltrafik WebServices
+PowerShell script för att interagera med AB Storstockholms Lokaltrafik WebServices - [API Realtid][api-realtid-link].
+
+Dessa funktioner låter dig söka realtidsinformation om avgångar från hållplatser i hela Stockholmsområdet via SLs realtids API. Mer information om API:et finns här
+
+http://www.trafiklab.se/api/sl-realtidsinfo
+
+För att kunna använda dessa API:er måste du ange en API-nyckel, du kan läsa mer om hur då skaffar en sådan på [Trafiklab.se][trafiklab-link].
+
+
+http://www.trafiklab.se/kom-igang
+
+[api-realtid-link]: http://www.trafiklab.se/api/sl-realtidsinfo
+[trafiklab-link]: http://www.trafiklab.se/kom-igang 
+
+# Förutsättningar
+
+För att använda dessa script måste man ha [PowerShell 3.0][powershell-wiki] eller senare installerad. PowerShell 3.0 är standard i Windows 8 men går utmärkt att [ladda ner][powershell3-link] till tidigare versioner av Windows.
+
+[powershell-wiki]: http://en.wikipedia.org/wiki/Windows_PowerShell
+[powershell3-link]: http://www.microsoft.com/en-us/download/details.aspx?id=29939
+
+# Hur gör man?
+
+Om du vill testa detta, ladda ner filerna och skaffa en API nyckel. SMidigast är att sedan spara denna API nyckel i din PowerShell profil. Från enkommandorad, skriv:
+
+PS C:\>notepad $PROFILE
+
+Sedan kan du lägga till följande rad i profilen:
+
+  $apiKey = 'xyz123xyz123xyz123xyz123xyz123zy'
+
+Gör man det är denna nyckel alltid tillgänlig men behöver inte sparas i scriptfilen. Glöm inte att ladda om profilen när du gjort detta! 
+
+PS C:\> . $PROFILE
+
+
+Nu kan du pröva att söka efter id för en hållplats;
+
+PS C:\> Get-SLRealtidSite -query Slussen -apiKey $apiKey
+
+
+Number                                                      Name
+------                                                      ----
+9192                                                        Slussen (Stockholm)
+
+
+
+För att se aktuella avgångar från Slussen, anropa DspDepartures metoden:
+
+
+PS C:\> Get-SLRealtidDpsDepartures -siteId 9192 -apiKey $apiKey
+
+
+xsi           : http://www.w3.org/2001/XMLSchema-instance
+xsd           : http://www.w3.org/2001/XMLSchema
+xmlns         : http://www1.sl.se/realtidws/
+LatestUpdate  : 2012-08-20T09:07:07.5045861+02:00
+ExecutionTime : 00:00:00.3749928
+Buses         : Buses
+Metros        :
+Trains        :
+Trams         : Trams
+
+
+Ett XML dokument som visar att det finns bussavgångar (Buses) och lokaltåg (Trams) Vill du direkt se filka bussar som snart åker från Slussen, skriv:
+
+PS C:\> (Get-SLRealtidDpsDepartures -siteId 9192 -apiKey $apiKey).Buses.DpsBus
+
+
+SiteId             : 9192
+StopAreaNumber     : 10149
+TransportMode      : BLUEBUS
+StopAreaName       : Slussen
+LineNumber         : 2
+Destination        : Sofia
+TimeTabledDateTime : 2012-08-20T09:08:00
+ExpectedDateTime   : 2012-08-20T09:08:49
+DisplayTime        : 0 min
+
+SiteId             : 9192
+StopAreaNumber     : 10149
+TransportMode      : BUS
+StopAreaName       : Slussen
+LineNumber         : 76
+Destination        : Frihamnen
+TimeTabledDateTime : 2012-08-20T09:09:00
+ExpectedDateTime   : 2012-08-20T09:09:00
+DisplayTime        : 0 min
+
+SiteId             : 9192
+StopAreaNumber     : 10149
+TransportMode      : BUS
+StopAreaName       : Slussen
+LineNumber         : 53
+Destination        : Roslagstull
+TimeTabledDateTime : 2012-08-20T09:06:00
+ExpectedDateTime   : 2012-08-20T09:09:12
+DisplayTime        : 0 min
+
+..
+..
+..
+
+
+
+# Kontakt 
+
+Om du har frågor om detta script, skapa helst ett [issue][issue-link] i det här projektet.
+
+[issue-link]: https://github.com/yooakim/ps-sl/issues
+
+
+# Historik
+
+	Datum		Version		Kommentar
+	==========	=======		============================================================
+	2012-08-20	0.1		Skapade koden
+
+
+
+
